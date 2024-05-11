89577da1a36eee07a6affbdbec5566542067d364
Michael <michaelcristopher21@gmail.com>
2024-05-11 00:56:50 -0500

    Crear URLs amigables

---
+++ controllers/principal/Blog.php
@@ -0,0 +1,10 @@
+<?php
+class Blog{
+    public function index(){
+        echo 'Mensaje desde el controlador';
+    }
+    public function listar(){
+        echo 'AQUI VAMOS A LISTAR LOS RESULTADOS DE LA BASE DE DATOS';
+    }
+}
+?>
