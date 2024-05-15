2725ef1d3732c36643b1e8734d8bdbca74cd6ef0
JDANiel2416 <N00287069@upn.pe>
2024-05-15 17:31:56 -0500

    avance 15-05

--- controllers/principal/Reserva.php
+++ controllers/principal/Reserva.php
@@ -7,8 +7,51 @@ class Reserva extends Controller
     }
     public function verify()
     {
-        $data['title'] = 'Reservas';
-        $data['subtitle'] = 'Verificar Disponibilidad';
-        $this->views->getView('principal/reservas', $data);
+        if (isset($_GET['f_llegada']) && isset($_GET['f_salida']) && isset($_GET['habitacion'])) {
+
+            $f_llegada = strClean($_GET['f_llegada']);
+            $f_salida = strClean($_GET['f_salida']);
+            $habitacion = strClean($_GET['habitacion']);
+
+            if (empty($f_llegada) || empty($f_salida) || empty($habitacion)) {
+                header('Location: ' . RUTA_PRINCIPAL . '?respuesta=warning');
+            } else {
+                $data['reserva'] = $this->model->getDisponible($f_llegada, $f_salida, $habitacion);
+                $data['title'] = 'Reservas';
+                $data['subtitle'] = 'Verificar Disponibilidad';
+                $data['disponible'] = [
+                    'f_llegada' => $f_llegada,
+                    'f_salida' => $f_salida,
+                    'habitacion' => $habitacion
+                ];
+                $this->views->getView('principal/reservas', $data);
+            }
+        }
+    }
+
+    public function listar($datos)
+    {
+        $array = explode(',', $datos);
+        $f_llegada = (!empty($array[0])) ? $array[0] : null;
+        $f_salida = (!empty($array[1])) ? $array[1] : null;
+        $habitacion = (!empty($array[2])) ? $array[2] : null;
+        $results = [];
+        if ($f_llegada != null && $f_salida != null && $habitacion  != null) {
+            $reservas = $this->model->getReservashabitacion($habitacion);
+            for ($i = 0; $i < count($reservas); $i++) {
+                $datos['id'] = $reservas[$i]['id'];
+                $datos['title'] = 'OCUPADO';
+                $datos['start'] = $reservas[$i]['fecha_ingreso'];
+                $datos['end'] = $reservas[$i]['fecha_salida'];
+                array_push($results, $datos);
+            }
+            $data['id'] = $habitacion;
+            $data['title'] = 'COMPROBANDO';
+            $data['start'] = $f_llegada;
+            $data['end'] = $f_salida;
+            array_push($results, $data);
+            echo json_encode($results, JSON_UNESCAPED_UNICODE);
+        }
+        die();
     }
 }
--- models/principal/PrincipalModel.php
+++ models/principal/PrincipalModel.php
@@ -1,27 +1,20 @@
 <?php
-class PrincipalModel extends Query{
+class PrincipalModel extends Query
+{
     private $con;
-    public function __construct() {
-        parent:: __construct();
+    public function __construct()
+    {
+        parent::__construct();
     }
 
-    public function getSliders(){
-        
+    public function getSliders()
+    {
+
         return $this->selectAll("SELECT * FROM sliders");
     }
 
-    public function getHabitaciones(){
-
+    public function getHabitaciones()
+    {
         return $this->selectAll("SELECT * FROM habitaciones");
-
     }
-
-    public function getDisponible($f_llegada, $f_salida, $habitacion){
-        return $this->selectAll("SELECT * FROM reservas 
-        WHERE fecha_ingreso <= '$f_salida' 
-        AND fecha_salida >= '$f_llegada' AND id_habitacion = $habitacion;
-        ");
-    }
-
 }
-?>
